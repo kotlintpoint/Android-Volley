6c1edd5888e1a31b23d7c75ef2da50bb618e4c5e
Ankit Sodha - 9033310919 <ankit.sodha123@gmail.com>
2016-08-02 15:07:06 +0530

    Update UrlConstants.java

--- main/java/realmcrud/sodhankit/com/myvolleysample/other/UrlConstants.java
+++ main/java/realmcrud/sodhankit/com/myvolleysample/other/UrlConstants.java
@@ -5,13 +5,13 @@ package realmcrud.sodhankit.com.myvolleysample.other;
  */
 public class UrlConstants {
 
-    public static final String BASE_URL="http://10.0.2.2:8080/TopsProject/myservice.php";
+    public static final String BASE_URL="http://10.0.2.2/PhpProject/myservice.php";
 
-    public static final String IMAGE_URL="http://10.0.2.2:8080/TopsProject/img6.png";
+    public static final String IMAGE_URL="http://10.0.2.2/PhpProject/imgage.png";
 
     public static final String JSON_OBJECT_REQUEST=BASE_URL+"?selectjsonobject";
 
-    public static final String JSON_ARRAY_REQUEST=BASE_URL+"?selectempall";
+    public static final String JSON_ARRAY_REQUEST=BASE_URL+"?selectjsonarray";
 
     public static final String SIMPLE_REQUEST=BASE_URL+"?insertemp";
 }
